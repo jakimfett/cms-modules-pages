a9d6abe4a9cd8be0d349c3aad13bd547211316b8
jakimfett <jakimfett@picraft.com>
2015-06-12 15:56:15 -0700

    Rough templates draft

--- classes/controller/pages.php
+++ classes/controller/pages.php
@@ -1,6 +1,45 @@
 <?php
 
-class Controller_Pages extends Controller_Template
-{
-    
+class Controller_Pages extends Controller_Template {
+
+    public $template = 'template/blank';
+    public $title;
+
+    /**
+     * Loads the template [View] object.
+     */
+    public function before() {
+        parent::before();
+
+        $this->title = $this->request->param('template');
+        if (empty($this->title)) {
+            $this->title = 'Labyrinth';
+        }
+
+        $this->template->header = View::factory('template/header');
+        $this->template->header->title = $this->title;
+        $this->template->footer = View::factory('template/footer');
+    }
+
+    public function action_index() {
+        
+        $this->template->body = View::factory('static/index');
+        
+        
+        $template = 'pages/' . $this->request->param('template', 'index');
+        
+        $post = Post::dcache(Path::lookup($template)['id'], 'page', Config::load('pages'));
+        
+        $this->template->body->promotext = $post->body;
+    }
+
+    public function after() {
+        if ($this->auto_render === TRUE) {
+            $body = $this->template->header->render();
+            $body .= $this->template->body->render();
+            $body .= $this->template->footer->render();
+            $this->response->body($body);
+        }
+    }
+
 }
