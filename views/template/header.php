53cd38f2e65c770594849cbde9a612790878f64d
jakimfett <jakimfett@picraft.com>
2015-06-13 21:36:55 -0700

    Include CSS stylesheet

--- views/template/header.php
+++ views/template/header.php
@@ -4,11 +4,13 @@
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta charset="UTF-8">
+        
+        <link rel="stylesheet" id="labyrinth-theme" href="./media/labyrinth/css/style.css" type="text/css" media="all">
 
         <?php foreach ($scripts as $script) : ?>
             <script type="text/javascript" src="<?= $script; ?>"></script>
         <?php endforeach; ?>
-
+            
         <title><?= $title ?></title>
 
     </head>
