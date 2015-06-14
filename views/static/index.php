0ff451b312fc58d9dd646beb263a120ea6cebe1b
jakimfett <jakimfett@picraft.com>
2015-06-13 22:35:09 -0700

    Add floating nav bar

--- views/static/index.php
+++ views/static/index.php
@@ -4,12 +4,23 @@
         <div id="header">
             <img id='logo' src="media/labyrinth/images/logo.png"/>
         </div>
-        <div id="navigation">
-
+        <div id='navigation-container'>
+            <div id="navigation">
+                <ul>
+                    <li><a href="index">Home</a></li>
+                    <li><a href="index/about">About</a></li>
+                    <li><a href="index/contact">Contact</a></li>
+                    <li><a href="index/faq">FAQ</a></li>
+                    <li><a href="index/tickets">Tickets</a></li>
+                </ul>
+            </div>
         </div>
         <div id="content">
             <?php if (isset($promotext)) : ?>
-                <?= trim($promotext); ?>
+                <?php for ($i = 0; $i < 120; $i++) : ?>
+                    <?= trim($promotext); ?>
+                <?php endfor; ?>
+
             <?php endif; ?>
         </div>
         <div id="footer">
