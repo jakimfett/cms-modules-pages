89a7033db6dfd81905d570aefe7a4710f01bbdd1
jakimfett <jakimfett@picraft.com>
2015-06-14 01:48:40 -0700

    Get text from CMS

--- classes/controller/pages.php
+++ classes/controller/pages.php
@@ -29,16 +29,33 @@ class Controller_Pages extends Controller_Template {
     }
 
     public function action_index() {
+        
+        $page = $this->request->param('template', 'index');
 
-        $this->template->body = View::factory('static/index');
+        $this->template->body = View::factory('static/' . $page);
 
+        switch ($page) {
+            case 'index':
+                $five_words_id = Path::lookup('pages/index-five-words')['id'];
+                $five_words_content = Post::dcache($five_words_id, 'page', Config::load('pages'));
+                $this->template->body->fivewords = $five_words_content->body;
 
-        $template = 'pages/' . $this->request->param('template', 'index');
-        $template_id = Path::lookup($template)['id'];
+                $elevator_pitch_id = Path::lookup('pages/index-elevator-pitch')['id'];
+                $elevator_pitch_content = Post::dcache($elevator_pitch_id, 'page', Config::load('pages'));
+                $this->template->body->pitch = $elevator_pitch_content->body;
 
-        if ($template_id) {
-            $post = Post::dcache($template_id, 'page', Config::load('pages'));
-            $this->template->body->promotext = $post->body;
+                $left_room_id = Path::lookup('pages/index-left-room')['id'];
+                $left_room_content = Post::dcache($left_room_id, 'page', Config::load('pages'));
+                $this->template->body->lroom = $left_room_content->body;
+
+                $right_room_id = Path::lookup('pages/index-right-room')['id'];
+                $right_room_content = Post::dcache($right_room_id, 'page', Config::load('pages'));
+                $this->template->body->rroom = $right_room_content->body;
+
+                break;
+
+            default:
+                break;
         }
     }
 
