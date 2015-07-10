d0043489f39000b5338b81b6867d12a02c20c126
jakimfett <jakimfett@picraft.com>
2015-07-09 21:58:38 -0700

    Add static video embed page

---
+++ views/static/video-embed.php
@@ -0,0 +1,24 @@
+<div id="fb-root">
+</div>
+<script>(
+            function (d, s, id) {
+                var js, fjs = d.getElementsByTagName(s)[0];
+                if (d.getElementById(id))
+                    return;
+                js = d.createElement(s);
+                js.id = id;
+                js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.3";
+                fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));
+</script>
+<div class="fb-video" data-allowfullscreen="1" data-href="/solvethelabyrinth/videos/vb.1386741058315070/1447348742254301/?type=1">
+    <div class="fb-xfbml-parse-ignore">
+        <blockquote cite="https://www.facebook.com/solvethelabyrinth/videos/1447348742254301/">
+            <a href="https://www.facebook.com/solvethelabyrinth/videos/1447348742254301/">Labyrinth</a>
+            <p>
+                
+            </p>
+            Posted by <a href="https://www.facebook.com/solvethelabyrinth">Labyrinth - Live Action Escape Games</a> on Saturday, June 27, 2015
+        </blockquote>
+    </div>
+</div>
