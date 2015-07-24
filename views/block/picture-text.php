59786bea30033f32f9d2b10b0e67403cb75cbbf3
jakimfett <jakimfett@picraft.com>
2015-07-23 19:43:00 -0700

    Add block for pictures with text

---
+++ views/block/picture-text.php
@@ -0,0 +1,12 @@
+<div class="picture-container picture-<?= $side; ?>">
+    <div class="picture-icon">
+        <i class="fa <?= $icon; ?>"></i>
+    </div>
+    <div class="text-container">
+        <h2><?= $title; ?></h2>
+        <p>
+            <?= $text; ?>
+        </p>
+    </div>
+    <div class="clear"></div>
+</div>
