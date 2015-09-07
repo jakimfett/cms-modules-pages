160c75a61b0f3ffe18289738c4aea107b3f3552a
jakimfett <jakimfett@gmail.com>
2015-09-06 20:59:17 -0700

    Update to 30 second video URL todo: move this to the CMS

--- views/block/video-embed.php
+++ views/block/video-embed.php
@@ -1,3 +1,3 @@
 <div class="media-embed">
-    <iframe width="560" height="315" src="https://www.youtube.com/embed/kFMHjG93_rc" frameborder="0" allowfullscreen></iframe>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/utXH8r7FzIs" frameborder="0" allowfullscreen></iframe>
 </div>
