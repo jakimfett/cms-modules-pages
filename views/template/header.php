93595ffb821d6251ada1ada071c8d1a32991a012
jakimfett <jakimfett@gmail.com>
2015-09-08 20:46:31 -0700

    Use absolute path for CSS locations

--- views/template/header.php
+++ views/template/header.php
@@ -5,8 +5,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta charset="UTF-8">
 
-        <link rel="stylesheet" id="labyrinth-theme" href="./media/labyrinth/css/style.css" type="text/css" media="all">
-        <link rel="stylesheet" href="./media/labyrinth/css/font-awesome.min.css">
+        <link rel="stylesheet" id="labyrinth-theme" href="/media/labyrinth/css/style.css" type="text/css" media="all">
+        <link rel="stylesheet" href="/media/labyrinth/css/font-awesome.min.css">
 
         <?php foreach ($scripts as $script) : ?>
             <script type="text/javascript" src="<?= $script; ?>"></script>
